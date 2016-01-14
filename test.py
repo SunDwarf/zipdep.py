05467f6ba0bd39f0cd86706bfbef5c1b2fc1ab88
SunDwarf <eyesismine@gmail.com>
2016-01-14 20:35:10 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import yaml
+import base64
+from tornado import auth
+print("Loaded YAML from {}".format(yaml.__path__))
+print("base64, however, is a built-in, so it's loaded from {}".format(base64.__file__))
+print("We only imported a function from tornado, not the module. However, that's checked too! Tornado's auth module is",
+      auth)
