a35923a8256f77ce92520e76af6d7d7dd7bb5eea
SunDwarf <eyesismine@gmail.com>
2016-01-15 19:27:40 +0000

    add base91 support alongside base85, if installed

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+import os
+
 import yaml
 import base64
 from tornado import auth
-from test2 import other_function
+try:
+    from test2 import other_function
+except ImportError as e:
+    __test2_e = e
+else:
+    __test2_e = "this doesn't work in the test environment."
+
 
 def test():
     pass
@@ -11,3 +19,6 @@ if __name__ == "__main__":
     print("base64, however, is a built-in, so it's loaded from {}".format(base64.__file__))
     print("We only imported a function from tornado, not the module. However, that's checked too! Tornado's auth module is",
           auth)
+    print("However, since other_function is a project file, it won't import successfully outside of the test "
+          "environment.")
+    print("See:", __test2_e)
